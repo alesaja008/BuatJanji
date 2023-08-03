614c8ffabb3b4a93c73b756c03432947695c98d1
Ales <alesaja008@gmail.com>
2023-08-03 09:07:58 +0700

    revisi footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoFooter from "../../assets/images/logo/logo-white.png";
 import "./styles.css";
+import { Row, Col } from "react-bootstrap";
 
 import {
   BiLogoFacebookCircle,
@@ -13,10 +14,15 @@ const Footer = () => {
     <>
       <section className="subscribe-input">
         <img src={LogoFooter} height={60} alt="logo" />
-        <p className="section-subtitle text-white">
-          Jl. Bendungan Hilir No.46, RT.8/RW.1, Bend. Hilir,Tanah Abang, Kota
-          Jakarta Pusat,Daerah Khusus Ibukota Jakarta 10210
-        </p>
+        <Row className="justify-content-center">
+          <Col lg={3}>
+            <p className="section-subtitle text-white">
+              Jl. Bendungan Hilir No.46, RT.8/RW.1, Bend. Hilir,Tanah Abang,
+              Kota Jakarta Pusat,Daerah Khusus Ibukota Jakarta 10210
+            </p>
+          </Col>
+        </Row>
+
         <div className="social-icon">
           <a href="#!">
             <BiLogoFacebookCircle className="icon" />
